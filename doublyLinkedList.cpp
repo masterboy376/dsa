d4f77c82a57ef0c2e6cbc34348ed29b5844a68f6
Sambhav Kaushik <92575005+masterboy376@users.noreply.github.com>
2022-09-03 05:30:19 +0000

    doubly linked list

---
+++ doublyLinkedList.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+using namespace std;
+
+
+struct Node {
+    int data;
+    struct Node* next;
+    struct Node* prev;
+};
+
+
+int main()
+{
+    /* code */
+    return 0;
+}
