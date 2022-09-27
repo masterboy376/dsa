eaa861cbf7da8cd475d977c0e606c74fbc33a47b
Sambhav Kaushik <92575005+masterboy376@users.noreply.github.com>
2022-09-27 05:58:27 +0000

    bst, search, insert

--- b-tree.cpp
+++ b-tree.cpp
@@ -1,59 +1,211 @@
 #include <iostream>
 using namespace std;
 
-struct Node{
+struct Node
+{
     int data;
-    struct Node * left;
-    struct Node * right;
+    struct Node *left;
+    struct Node *right;
 };
 
-struct Node* createNode(int data){
-    struct Node* n;
+struct Node *createNode(int data)
+{
+    struct Node *n;
     n = new struct Node();
-    n->data=data;
-    n->left=NULL;
-    n->right=NULL;
+    n->data = data;
+    n->left = NULL;
+    n->right = NULL;
     return n;
 }
 
-void preTraversal(struct Node* root){
-    if(root!=NULL){
-        cout<<root->data<<" ";
+void preTraversal(struct Node *root)
+{
+    if (root != NULL)
+    {
+        cout << root->data << " ";
         preTraversal(root->left);
         preTraversal(root->right);
     }
 }
 
-void postTraversal(struct Node* root){
-    if(root!=NULL){
+void postTraversal(struct Node *root)
+{
+    if (root != NULL)
+    {
         postTraversal(root->left);
         postTraversal(root->right);
-        cout<<root->data<<" ";
+        cout << root->data << " ";
     }
 }
-void inTraversal(struct Node* root){
-    if(root!=NULL){
+void inTraversal(struct Node *root)
+{
+    if (root != NULL)
+    {
         inTraversal(root->left);
-        cout<<root->data<<" ";
+        cout << root->data << " ";
         inTraversal(root->right);
     }
 }
+int isBst(struct Node *root)
+{
+    static struct Node *prev = NULL;
+    if (root != NULL)
+    {
+        if (!isBst(root->left))
+        {
+            return 0;
+        }
+        if (prev != NULL && root->data <= prev->data)
+        {
+            return 0;
+        }
+        prev = root;
+        return isBst(root->right);
+    }
+    else
+    {
+        return 1;
+    }
+}
+struct Node *isPresentRecursive(struct Node *root, int n)
+{
+    if (root == NULL)
+    {
+        return NULL;
+    }
+    if (n == root->data)
+    {
+        return root;
+    }
+    else if (n < root->data)
+    {
+        return isPresentRecursive(root->left, n);
+    }
+    else
+    {
+        return isPresentRecursive(root->right, n);
+    }
+}
+struct Node *isPresentIterative(struct Node *root, int n)
+{
+    while (root != NULL)
+    {
+        if (n == root->data)
+        {
+            return root;
+        }
+        else if (n < root->data)
+        {
+            root = root->left;
+        }
+        else
+        {
+            root = root->right;
+        }
+    }
+    return NULL;
+}
+void insertion(struct Node *root, int n)
+{
+    // struct Node* prev =;
+    while (root != NULL)
+    {
+        if (n == root->data)
+        {
+            cout << "The item already in BST" << endl;
+        }
+        else if (n < root->data)
+        {
+            if (root->left != NULL)
+            {
+                root = root->left;
+            }
+            else
+            {
+                struct Node *newNode = createNode(n);
+                root->left = newNode;
+            }
+        }
+        else
+        {
+            if (root->right != NULL)
+            {
+                root = root->right;
+            }
+            else
+            {
+                struct Node *newNode = createNode(n);
+                root->right = newNode;
+            }
+        }
+    }
+}
 
-int main(){
+int main()
+{
 
-    struct Node* root = createNode(2); 
-    struct Node* first = createNode(1); 
-    struct Node* second = createNode(4); 
+    struct Node *root = createNode(12);
+    struct Node *p1 = createNode(6);
+    struct Node *p2 = createNode(18);
+    struct Node *p3 = createNode(3);
+    struct Node *p4 = createNode(11);
+    struct Node *p5 = createNode(14);
 
-    root->left = first;
-    root->right = second;
+    root->left = p1;
+    root->right = p2;
+
+    p1->left = p3;
+    p1->right = p4;
+
+    p2->left = p5;
+
+    //     12
+    //    /  \
+        //   6    18
+    //  / \   /
+    // 3  11 14
 
     preTraversal(root);
-    cout<<endl;
+    cout << endl;
     postTraversal(root);
-    cout<<endl;
+    cout << endl;
     inTraversal(root);
-    cout<<endl;
+    cout << endl;
+
+    // check bst
+    if (isBst(root))
+    {
+        cout << "is BST" << endl;
+    }
+    else
+    {
+        cout << "not a BST" << endl;
+    }
+
+    // recursive search
+    if (isPresentRecursive(root, 19) != NULL)
+    {
+        cout << "is present" << endl;
+    }
+    else
+    {
+        cout << "not present" << endl;
+    }
+
+    // iteraive search
+    if (isPresentIterative(root, 18) != NULL)
+    {
+        cout << "is present" << endl;
+    }
+    else
+    {
+        cout << "not present" << endl;
+    }
+
+    // insertion
+    insertion(root, 19);
+    inTraversal(root);
+    cout << endl;
     
     return 0;
 }
